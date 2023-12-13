1c62ce694b58350e80655b1d0bcb804c04b7a2ca
Isaque Ribeiro Fererira <38390651+isaquerr25@users.noreply.github.com>
2023-12-13 17:39:32 -0400

    translate

--- main_test.go
+++ main_test.go
@@ -11,55 +11,55 @@ import (
 )
 
 func TestCreateUser(t *testing.T) {
-	// Configurar o ambiente de teste (por exemplo, um banco de dados de teste separado)
-	// Certifique-se de restaurar o ambiente ao seu estado original após os testes
+	// Set up the test environment (e.g., a separate test database)
+	// Ensure to restore the environment to its original state after tests
 
-	// Criar um servidor Gin com as mesmas rotas
+	// Create a Gin server with the same routes
 	router := gin.Default()
 	router.POST("/users", CreateUser)
 
-	// Criar um request de teste para a rota de criação de usuário
+	// Create a test request for the user creation route
 	reqBody := strings.NewReader(`{"name": "John Doe", "email": "john@example.com"}`)
 	req, err := http.NewRequest("POST", "/users", reqBody)
 	assert.NoError(t, err)
 
-	// Simular uma solicitação HTTP
+	// Simulate an HTTP request
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	// Verificar se a resposta HTTP é 200 OK
+	// Check if the HTTP response is 200 OK
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// Verificar se o corpo da resposta contém os dados do usuário criado
+	// Check if the response body contains the created user's data
 	assert.Contains(t, w.Body.String(), "John Doe")
 	assert.Contains(t, w.Body.String(), "john@example.com")
 
-	// Adicionar mais verificações conforme necessário
+	// Add more checks as needed
 
-	// Limpar o banco de dados após os testes
-	// Substitua essa parte com a lógica específica do seu banco de dados
-	// Por exemplo, para GORM, você pode usar db.DropTableIfExists(&User{})
-	// Certifique-se de que essa lógica esteja adaptada para o seu caso específico
-	// (não use em ambientes de produção sem cuidado)
+	// Clean up the database after tests
+	// Replace this part with the specific logic for your database
+	// For example, for GORM, you can use db.DropTableIfExists(&User{})
+	// Ensure that this logic is adapted to your specific case
+	// (do not use in production environments without caution)
 }
 
 func TestGetUsers(t *testing.T) {
-	// Configurar o ambiente de teste (por exemplo, um banco de dados de teste separado)
+	// Set up the test environment (e.g., a separate test database)
 
-	// Criar um servidor Gin com as mesmas rotas
+	// Create a Gin server with the same routes
 	router := gin.Default()
 	router.GET("/users/:id", GetUsers)
 
-	// Criar um request de teste para a rota de obtenção de usuários
+	// Create a test request for the user retrieval route
 	req, err := http.NewRequest("GET", "/users/1", nil)
 	assert.NoError(t, err)
 
-	// Simular uma solicitação HTTP
+	// Simulate an HTTP request
 	w := httptest.NewRecorder()
 	router.ServeHTTP(w, req)
 
-	// Verificar se a resposta HTTP é 200 OK
+	// Check if the HTTP response is 200 OK
 	assert.Equal(t, http.StatusOK, w.Code)
 
-	// Adicionar mais verificações conforme necessário
+	// Add more checks as needed
 }
